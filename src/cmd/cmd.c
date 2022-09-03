76309e14535b8599928ab90ec03417607b53ecfe
pt <petarturukalo@gmail.com>
2022-09-03 12:33:22 +1000

    cmd approx search

--- src/cmd/cmd.c
+++ src/cmd/cmd.c
@@ -11,15 +11,58 @@ void cmds_free(cmds_t *c)
 	free(c->htbl);
 }
 
+/*
+ * The current minimum and maximum lengths of a short name in the cmd_t struct. 
+ * The max is subject to change if a command with a longer short name is added.
+ */
+#define MIN_SHORT_NAME_LEN 1
+#define MAX_SHORT_NAME_LEN 2
+
+/*
+ * Look up a command by name using an approximate search. This allows the user to
+ * enter a command name inbetween its short name and long name and have it resolve
+ * to the short name.
+ *
+ * For example, the command close has short name "c" and long name "close". An approximate
+ * search has the user type a string matching regex pattern c.* resolve to the close command: 
+ * the user could type "cl", "cls", or even "cjjj", etc.
+ *
+ * Because some commands have short names with 2 characters, prefixes are searched for in descending 
+ * order of length: first the prefix of length 2 is checked, then the prefix of length 1. The "fclose"
+ * command for instance has prefix "fc" of length 2. If a new command "foo" was added in the future with
+ * short name "f", if prefixes weren't searched for in descending order of length, then entering "fclo" would
+ * first check for a prefix of length 1 "f", and resolve to command "foo", when it should have instead
+ * resolved to "fclose". Searching in descending order of prefix length fixes this.
+ */
+cmd_t *cmds_search_approx(cmds_t *c, char *name)
+{
+	if (strlen(name) > MIN_SHORT_NAME_LEN) {
+		ENTRY e, *ep;
+		char name_prefix[MAX_SHORT_NAME_LEN+1];
+		int prefix_len;
+
+		name_prefix[0] = '\0';
+		strncat(name_prefix, name, MAX_SHORT_NAME_LEN);
+		e.key = name_prefix;
+
+		prefix_len = strlen(name_prefix);
+		for (; prefix_len > 0; --prefix_len) {
+			name_prefix[prefix_len] = '\0';
+			if (hsearch_r(e, FIND, &ep, c->htbl))
+				return (cmd_t *)ep->data;
+		}
+	}
+	return NULL;
+}
+
 cmd_t *cmds_search(cmds_t *c, char *name)
 {
 	ENTRY e, *ep;
 
 	e.key = name;
-
 	if (hsearch_r(e, FIND, &ep, c->htbl))
 		return (cmd_t *)ep->data;
-	return NULL;
+	return cmds_search_approx(c, name);
 }
 
 void cmds_init(cmds_t *cs)
